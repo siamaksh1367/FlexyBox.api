2f0c57b66d5c22809d52fdb7e936a382f0f0798a
sish-siameh <sish@siameh.com>
2024-10-10 17:31:44 +0200

    add dbcontext and configuration and okta configuration

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using FlexyBox.common;
+using FlexyBox.dal.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 
 namespace FlexyBox.api
 {
@@ -7,16 +11,33 @@ namespace FlexyBox.api
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.Configuration.AddEnvironmentVariables();
+            builder.Configuration.AddUserSecrets("ac67d5f4-4690-491a-a0d9-b99948807318");
+            var okta = builder.Configuration.GetSection(nameof(Okta)).Get<Okta>();
+            var sql = builder.Configuration.GetSection(nameof(SQLConnectionString)).Get<SQLConnectionString>();
+
+            builder.Services.Configure<Okta>(builder.Configuration.GetSection(nameof(Okta)));
+            builder.Services.Configure<SQLConnectionString>(builder.Configuration.GetSection(nameof(SQLConnectionString)));
+            builder.Services.Configure<StorageConnectionString>(builder.Configuration.GetSection(nameof(StorageConnectionString)));
+
+            builder.Services.AddDbContext<FlexyBoxDB>(option => option.UseSqlServer(sql.ConnectionString));
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = okta.Authority;
+                options.Audience = okta.Audience;
+            });
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
